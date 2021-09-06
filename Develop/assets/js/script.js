a9dd72b985e947d3cd7f8697c2f90dc360b31b4b
parappa20 <88298540+james182-dang@users.noreply.github.com>
2021-09-06 18:13:56 -0400

    set up save and load functions, time block color code functionality

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -4,5 +4,65 @@ setInterval(function () {
     $('#currentDay').html("The current time and date is " + rightNow);
 }, 1000);
 
+//save tasks to task blocks
+function saveNine() {
+    localStorage.setItem("nineAm", $('#nineAm').value);
+};
 
-//Connect time blocks to real-world-time
+function saveTen() {
+    localStorage.setItem("tenAm", $('#tenAm').value);
+};
+
+function saveEleven() {
+    localStorage.setItem("elevenAm", $('#elevenAm').value);
+};
+
+function saveTwelve() {
+    localStorage.setItem("twelvePm", $('#twelvePm').value);
+};
+
+function saveOne() {
+    localStorage.setItem("onePm", $('#onePm').value);
+};
+
+function saveTwo() {
+    localStorage.setItem("twoPm", $('#twoPm').value);
+};
+
+function saveThree() {
+    localStorage.setItem("threePm", $('#threePm').value);
+};
+
+function saveFour() {
+    localStorage.setItem("fourPm", $('#fourPm').value);
+};
+
+function saveFive() {
+    localStorage.setItem("fivePm", $('#fivePm').value);
+};
+
+//function to load text into schedule blocks
+function loadTasks() {
+    localStorage.getItem("nineAm");
+    localStorage.getItem("tenAm");
+    localStorage.getItem("elevenAm");
+    localStorage.getItem("twelvePm");
+    localStorage.getItem("onePm");
+    localStorage.getItem("twoPm");
+    localStorage.getItem("threePm");
+    localStorage.getItem("fourPm");
+    localStorage.getItem("fivePm");
+};
+
+// timeblock color coding
+let hour = $(".hour");
+
+let time = moment(hour, "L").set("hour", 17);
+
+if (moment().isBefore(time)) {
+    $(".hour").addClass("future");
+} else if (Math.abs(moment().diff(time, "hours")) <= 1) {
+    $(".hour").addClass("present");
+} else {
+    $(".hour").addClass("past");
+};
